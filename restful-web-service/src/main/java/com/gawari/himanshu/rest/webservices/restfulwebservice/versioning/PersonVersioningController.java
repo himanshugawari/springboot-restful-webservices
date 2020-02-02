4c6022b7b0ffc4bac36872cabe8407f149542894
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-02-02 19:30:03 +0530

    Accept Header Versioning

--- restful-web-service/src/main/java/com/gawari/himanshu/rest/webservices/restfulwebservice/versioning/PersonVersioningController.java
+++ restful-web-service/src/main/java/com/gawari/himanshu/rest/webservices/restfulwebservice/versioning/PersonVersioningController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 
-	// Basic Versioning
+	// URI versioning
 	@GetMapping(path = "/v1/person")
 	public PersonV1 personV1() {
 		return new PersonV1("Himanshu Gawari");
 	}
 
-	// Basic Versioning
+	// URI versioning
 	@GetMapping(path = "/v2/person")
 	public PersonV2 personV2() {
 		return new PersonV2(new Name("Himanshu", "Gawari"));
@@ -34,7 +34,7 @@ public class PersonVersioningController {
 
 	// http://localhost:8080/person/header
 	// pass version=1 as headers
-	// versioning using request header
+	// versioning using request header or MIME type versioning
 	@GetMapping(path = "/person/header", headers = "version=1")
 	public PersonV1 headerV1() {
 		return new PersonV1("Himanshu Gawari");
@@ -42,9 +42,27 @@ public class PersonVersioningController {
 
 	// http://localhost:8080/person/header
 	// pass version=2 as headers
-	// versioning using request header
+	// versioning using request header or MIME type versioning
 	@GetMapping(path = "/person/header", headers = "version=2")
 	public PersonV2 headerV2() {
+		return new PersonV2(new Name("Himanshu", "Gawari"));
+	}
+
+	// http://localhost:8080/person/produces
+	// pass application/vnd.company.app-v1+json as headers
+	// versioning using produces
+	// called Accept Header Versioning
+	@GetMapping(path = "/person/produces", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 produceV1() {
+		return new PersonV1("Himanshu Gawari");
+	}
+
+	// http://localhost:8080/person/produces
+	// pass application/vnd.company.app-v2+json as headers
+	// versioning using produces
+	// called Accept Header Versioning
+	@GetMapping(path = "/person/produces", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 produceV2() {
 		return new PersonV2(new Name("Himanshu", "Gawari"));
 	}
 }
