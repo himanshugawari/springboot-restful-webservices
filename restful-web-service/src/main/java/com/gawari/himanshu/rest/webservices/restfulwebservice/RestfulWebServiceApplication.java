cc35980ac89dbd12343eaa1a854621ba7cbd6746
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-02-02 12:30:00 +0530

    internationalization addd

--- restful-web-service/src/main/java/com/gawari/himanshu/rest/webservices/restfulwebservice/RestfulWebServiceApplication.java
+++ restful-web-service/src/main/java/com/gawari/himanshu/rest/webservices/restfulwebservice/RestfulWebServiceApplication.java
@@ -1,7 +1,12 @@
 package com.gawari.himanshu.rest.webservices.restfulwebservice;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
@@ -10,4 +15,22 @@ public class RestfulWebServiceApplication {
 		SpringApplication.run(RestfulWebServiceApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		// SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	// this can also be set in application.properties
+	// spring.messages.basename=messages
+	/*
+	 * // method name should be messageSource() nothing else
+	 * 
+	 * @Bean public ResourceBundleMessageSource messageSource() {
+	 * ResourceBundleMessageSource messageSource = new
+	 * ResourceBundleMessageSource(); messageSource.setBasename("messages"); return
+	 * messageSource; }
+	 */
 }
--- restful-web-service/src/main/java/com/gawari/himanshu/rest/webservices/restfulwebservice/helloworld/HelloWorldController.java
+++ restful-web-service/src/main/java/com/gawari/himanshu/rest/webservices/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.gawari.himanshu.rest.webservices.restfulwebservice.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 //@Controller
 @RestController
 public class HelloWorldController {
+
+	@Autowired
+	private MessageSource messageSource;
 
 	// @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
 	@GetMapping(path = "/hello-world")
@@ -24,4 +30,30 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
+
+	/*
+	 * @GetMapping(path = "/hello-world-internationalized") public String
+	 * helloWorldInternationalized(
+	 * 
+	 * @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+	 * return messageSource.getMessage("good.morning.message", null, locale); }
+	 */
+
+	/*
+	 * @GetMapping(path = "/hello-world-internationalized") public String
+	 * helloWorldInternationalized(
+	 * 
+	 * @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+	 * // return "Good Morning"; // return
+	 * messageSource.getMessage("good.morning.message", null, //
+	 * LocaleContextHolder.getLocale()); return
+	 * messageSource.getMessage("good.morning.message", null, locale); }
+	 */
+
+	// simplified Internationalization
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
+	}
+
 }
